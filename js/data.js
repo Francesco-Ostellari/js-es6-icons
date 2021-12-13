83fa060f6e9a2b7cd4e5f526e167ba8e0b074320
Francesco-Ostellari <francescoostellari@gmail.com>
2021-12-13 16:24:22 +0100

    add-function-cards

--- js/data.js
+++ js/data.js
@@ -1,4 +1,11 @@
-[
+/* Milestone 1
+Partendo dalla struttura dati fornita, visualizzare in pagina un box per ogni icona, in cui è presente il nome dell'icona e l'icona stessa.
+	Milestone 2
+Ciascuna icona ha una proprietà "color": utilizzare questa proprietà per visualizzare le icone del colore corrispondente.
+	Milestone 3
+Aggiungere alla pagina una select in cui le options corrispondono ai vari tipi di icone(animal, vegetable, user).Quando l'utente seleziona un tipo dalla select, visualizzare solamente le icone corrispondenti. */
+
+const card = [
 	{
 		name: 'cat',
 		prefix: 'fa-',
@@ -112,3 +119,24 @@
 		color: 'blue'
 	}
 ];
+
+function printCards(arrayList, container) {
+	container.innerHTML = '';
+	for (let index = 0; index < arrayList.length; index++) {
+		const object = arrayList[index];
+
+		const layoutDiv = 
+	 `<ul class="flex ul">
+      <li class="card flex">
+          <i class="${object.family} ${object.prefix}${object.name} animals" style ="color:${object.color}"></i><span>${object.name}</span>
+      </li>
+    </ul>`;
+		console.log(layoutDiv);
+		container.innerHTML += layoutDiv;
+	}
+}
+
+const container = document.querySelector('.cards-container');
+printCards(card, container);
+
+console.log(card);
